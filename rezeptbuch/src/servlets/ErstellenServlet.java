34c30138cf8dacc59ec55c8bec2c34bf644269b6
Flo <Flo@Flo-PC.fritz.box>
2017-04-23 18:39:23 +0200

    Rezept erstellen als HTML Datei sowie das zugehörige Servlet erstellt, Servlet wird schon aufgerufen über die html, jedoch werden Parameter noch nicht übergeben

---
+++ rezeptbuch/src/servlets/ErstellenServlet.java
@@ -0,0 +1,44 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ErstellenServlet")
+public class ErstellenServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	
+		request.setCharacterEncoding("UTF-8");
+		
+		// Parameter von dem Request werden geholt
+		String zutaten = request.getParameter("zutaten");
+		final String beschreibung = request.getParameter("beschreibung");
+		
+		// final String bild = request.getParameter("bild");
+		
+		// Response vorbereiten
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		final PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h4>" + zutaten + "und" + beschreibung + " Beschreibung<h4>");
+		out.println("</body>");
+		out.println("</html>");
+			
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		doGet(request, response);
+		
+	}
+}
