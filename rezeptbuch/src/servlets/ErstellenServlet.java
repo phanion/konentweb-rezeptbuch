d5c4392fad57bfb278a8b087ea48a164384cbcc1
Flo <Flo@10.30.33.46>
2017-04-24 17:26:45 +0200

    Änderung im Servlet wg. fehlender Parameter

--- rezeptbuch/src/servlets/ErstellenServlet.java
+++ rezeptbuch/src/servlets/ErstellenServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,23 +16,23 @@ public class ErstellenServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 	
-		request.setCharacterEncoding("UTF-8");
+		//request.setCharacterEncoding("UTF-8");
 		
 		// Parameter von dem Request werden geholt
-		String zutaten = request.getParameter("zutaten");
+		final String zutat = request.getParameter("zutaten");
 		final String beschreibung = request.getParameter("beschreibung");
 		
 		// final String bild = request.getParameter("bild");
 		
 		// Response vorbereiten
-		response.setContentType("text/html");
+		//response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8");
 		
 		final PrintWriter out = response.getWriter();
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h4>" + zutaten + "und" + beschreibung + " Beschreibung<h4>");
+		out.println("<h4>" + zutat + " und " + beschreibung + "<h4>");
 		out.println("</body>");
 		out.println("</html>");
 			
