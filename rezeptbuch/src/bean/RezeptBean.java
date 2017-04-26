f85662a422d4e005595200a464403db0749ef614
Michael Phan <michael.phan@zoho.com>
2017-04-26 20:51:58 +0200

    Nachtrag Rezept-Zutaten

--- rezeptbuch/src/bean/RezeptBean.java
+++ rezeptbuch/src/bean/RezeptBean.java
@@ -9,7 +9,7 @@ public class RezeptBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private String rezept;
-	private String zutaten;
+	private String zutatenText;
 	private String beschreibung;
 
 	private List<Zutat> zutatenListe;
@@ -18,8 +18,8 @@ public class RezeptBean implements Serializable {
 	public String getRezept() { return rezept; }
 	public void setRezept(String rezept) { this.rezept = rezept; }
 
-	public String getZutaten() { return zutaten; }
-	public void setZutaten(String zutaten) { this.zutaten = zutaten; }
+	public String getZutatenText() { return zutatenText; }
+	public void setZutatenText(String zutaten) { this.zutatenText = zutaten; }
 
 	public String getBeschreibung() { return beschreibung; }
 	public void setBeschreibung(String beschreibung) { this.beschreibung = beschreibung; }
