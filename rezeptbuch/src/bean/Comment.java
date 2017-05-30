12d05cdf9d81cbd70982f318a57e7ff76080f4bb
Michael Phan <michael.phan@zoho.com>
2017-05-30 23:13:40 +0200

    Verschieben der Bean-Klassen aus dem Package 'classes' in das Package 'bean'. Löschen des leeren Packages 'classes'.

--- rezeptbuch/src/bean/Comment.java
+++ rezeptbuch/src/bean/Comment.java
@@ -1,18 +1,10 @@
-package classes;
-
-import bean.RezeptBean;
-import bean.User;
+package bean;
 
 public class Comment {
 	private Long id;
 	private User author;
 	private String comment;
 	private RezeptBean receipe;
-	
-	
-	
-	
-	
 
 	public Comment(Long id, User author, String comment, RezeptBean receipe) {
 		super();
@@ -21,30 +13,37 @@ public class Comment {
 		this.comment = comment;
 		this.receipe = receipe;
 	}
+
 	public User getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(User author) {
 		this.author = author;
 	}
+
 	public String getComment() {
 		return comment;
 	}
+
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
+
 	public RezeptBean getReceipe() {
 		return receipe;
 	}
+
 	public void setReceipe(RezeptBean receipe) {
 		this.receipe = receipe;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	
+
 }
--- rezeptbuch/src/bean/Ingredient.java
+++ rezeptbuch/src/bean/Ingredient.java
@@ -2,55 +2,52 @@
  * Autor: Lorenz
  */
 
-package classes;
+package bean;
 
 public class Ingredient {
-	
+
 	private Integer quantity;
 	private String unit;
 	private String ingredient;
-	
-	
-	
+
 	public Ingredient(String ingredient, Integer quantity, String unit) {
 		super();
 		this.ingredient = ingredient;
 		this.quantity = quantity;
 		this.unit = unit;
 	}
-	
-	
+
 	public String getIngredient() {
 		return ingredient;
 	}
+
 	public void setIngredient(String ingredient) {
 		this.ingredient = ingredient;
 	}
+
 	public Integer getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
+
 	public String getUnit() {
 		return unit;
 	}
+
 	public void setUnit(String unit) {
 		this.unit = unit;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Ingredient [quantity=" + quantity + ", unit=" + unit + ", ingredient=" + ingredient + "]";
 	}
-	
-	public String outputString(){
+
+	public String outputString() {
 		return quantity + " " + unit + " " + ingredient;
 	}
-	
-	
-	
-	
-	
+
 }
