387e4892fa0182cff8892520b1c074cf475e2d10
Lorenz <LorenzFo>
2017-06-14 01:01:53 +0200

    Seite zum Anzeigen der eigenen Rezepte

---
+++ rezeptbuch/WebContent/jsp/ownRecipes.jsp
@@ -0,0 +1,39 @@
+<!-- Autor: Lorenz -->
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="main.css">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Rezeptbuch - Ihre Rezepte</title>
+</head>
+<body>
+	<%@ include file="fragments/nav.jspf"%>
+	<main>
+	<h1>Ihre Rezepte</h1>
+	<br>
+	<c:forEach items="${recipes}" var="recipe">
+		<div class="search-entry clearfix">
+
+			<h2>
+				<a href="/rezeptbuch/LoadRecipeServlet?id=${recipe.id}">${recipe.name}</a>
+			</h2>
+			<dl>
+				<dd>Bewertung</dd>
+				<dt>
+					<c:choose>
+						<c:when test="${recipe.ratingCount == 0}">-</c:when>
+						<c:otherwise>${recipe.ratingSum / recipe.ratingCount}</c:otherwise>
+					</c:choose>
+				</dt>
+
+
+
+			</dl>
+		</div>
+	</c:forEach> </main>
+</body>
+</html>
---
+++ rezeptbuch/src/servlets/LoadOwnRecipesServlet.java
@@ -0,0 +1,99 @@
+/**
+ * @author Lorenz
+ */
+
+package servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import bean.RezeptBean;
+import bean.User;
+
+/**
+ * Servlet implementation class LoadOwnRecipesServlet
+ */
+@WebServlet("/LoadOwnRecipesServlet")
+public class LoadOwnRecipesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoadOwnRecipesServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Resource(lookup = "jdbc/MyRezeptbuchPool")
+	private DataSource ds;
+    
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		
+		try {
+			List<RezeptBean> recipes = loadRecipes((User) session.getAttribute("user"));
+			request.setAttribute("recipes", recipes);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher disp = request.getRequestDispatcher("/jsp/ownRecipes.jsp");
+		disp.forward(request, response);
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	public List<RezeptBean> loadRecipes(User user) throws SQLException{
+		List<RezeptBean> recipes = new ArrayList<RezeptBean>();
+		
+		final Connection con = ds.getConnection();
+		PreparedStatement ps = con.prepareStatement("SELECT * FROM recipes WHERE creator=?");
+		ps.setLong(1, user.getID());
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()){
+			RezeptBean recipe = new RezeptBean();
+			recipe.setId(rs.getLong("ID"));
+			recipe.setName(rs.getString("name"));
+			recipe.setRatingCount(rs.getInt("ratingCount"));
+			recipe.setRatingSum(rs.getInt("ratingSum"));
+			
+			recipes.add(recipe);
+			
+		}
+		
+		return recipes;
+	}
+
+}
